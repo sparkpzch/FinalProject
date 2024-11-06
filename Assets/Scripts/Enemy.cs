85e4b560ebb51319e769515ee07d8305891734ef
Chanuwat-Damsanit <chanuwat.dams@bumail.net>
2024-11-06 20:30:10 +0700

    Modify Enemy Scripts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Enemy : Character
 {
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private CircleCollider2D enemyHitBox;
+
+
     void Start()
     {
-
+        rb2d = GetComponent<Rigidbody2D>();
+        enemyHitBox = GetComponent<CircleCollider2D>();
+        health = 1;
     }
 
     void Update()
